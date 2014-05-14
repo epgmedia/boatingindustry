28a373fa195de8c44508402d5cb86c85977b3be0
Christopher Gerber <chriswgerber@gmail.com>
2014-05-14 12:24:32 -0500

    Stupid Wordpress

--- wp-content/themes/gopublish/inc/tablecloth/table-cloth-functions.php
+++ wp-content/themes/gopublish/inc/tablecloth/table-cloth-functions.php
@@ -13,8 +13,8 @@ define( 'TC_INC_URL', plugin_dir_url( __FILE__ ) );
 function table_cloth_tables() {
     if ( is_page_template( 'inc/page-table-cloth.php' ) ) {
         /** Call enqueue */
-        wp_enqueue_style( 'table-cloth-css', TC_INC_URL . "/css/tablecloth.css" );
-        wp_enqueue_script( 'table-cloth-js', TC_INC_URL . "/js/tablecloth.js" );
+        wp_enqueue_style( 'table-cloth-css', TC_INC_URL . "css/tablecloth.css" );
+        wp_enqueue_script( 'table-cloth-js', TC_INC_URL . "js/tablecloth.js" );
     }
 }
 add_action( 'wp_enqueue_scripts', 'table_cloth_tables' );
