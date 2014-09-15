facca3e5be517d665938af738d1f3944c374e294
Christopher Gerber <chriswgerber@gmail.com>
2014-09-15 16:08:56 -0500

    New interstitial ad.

--- wp-content/themes/gopublish/leaderboardhead.php
+++ wp-content/themes/gopublish/leaderboardhead.php
@@ -1,66 +1,56 @@
 <?php if ( get_theme_mod( 'display-leader' ) == "Yes" ) { ?>
 
-	<div id="leaderboard">
+	<div id="leaderboard" class="leaderboardAd">
 
-	<?php if ( get_theme_mod( 'leaderad-type' ) == "Static Image" ) {
+		<?php if ( get_theme_mod( 'leaderad-type' ) == "Static Image" ) {
 
-		$leaderurl   = get_theme_mod( 'leader-url' );
-		$leaderimage = get_theme_mod( 'leader-image' );
+			$leaderurl   = get_theme_mod( 'leader-url' );
+			$leaderimage = get_theme_mod( 'leader-image' );
 
-		if ( NULL !== $leaderurl ) { ?>
+			if ( NULL !== $leaderurl ) { ?>
+				<a target="_blank" href="<?php echo $leaderurl; ?>">
+			<?php }
 
-			<a target="_blank" href="<?php echo $leaderurl; ?>">
+			if ( NULL !== $leaderimage ) { ?>
+				<img src="<?php echo $leaderimage; ?>" class="leaderimage" />
+			<?php }
 
-		<?php }
+			if ( NULL !== $leaderurl ) { ?>
+				</a>
+			<?php }
 
-		if ( NULL !== $leaderimage ) { ?>
+		} else if ( get_theme_mod( 'leaderad-type' ) == "Ad Tag" ) {
 
-			<img src="<?php echo $leaderimage; ?>" class="leaderimage" />
+			//SWW Mod to target ads for Top 100 & MDCE sections
+			$openxcode = get_theme_mod('openx-code');
+			$sww_uri   = strtolower($_SERVER["REQUEST_URI"]);
 
-		<?php }
+			if ( strpos( $sww_uri,'/top-100/' ) !== false )  {
+				$searchstr  = "div-gpt-ad-1398366141837";
+				$replacestr = "div-gpt-ad-1398366494877";
+				$openxcode  = str_ireplace($searchstr, $replacestr, $openxcode);
 
-		if (NULL !== $leaderurl ) { ?>
+				$searchstr  = "BIM_ROS";
+				$replacestr = "BIM_T100";
+				$openxcode  = str_ireplace($searchstr, $replacestr, $openxcode);
+			}
 
-			</a>
+			if (
+				strpos( $sww_uri,'/mdce/' ) !== false ||
+				strpos( $sww_uri,'/category/marine-dealer-conference/' ) !== false
+			) {
+				$searchstr  = "div-gpt-ad-1398366141837";
+				$replacestr = "div-gpt-ad-1398366605405";
+				$openxcode  = str_ireplace($searchstr, $replacestr, $openxcode);
 
-		<?php }
+				$searchstr  = "BIM_ROS";
+				$replacestr = "BIM_MDCE";
+				$openxcode  = str_ireplace($searchstr, $replacestr, $openxcode);
+			}
 
-	} else if ( get_theme_mod( 'leaderad-type' ) == "Ad Tag" ) {
-
-		//SWW Mod to target ads for Top 100 & MDCE sections
-		$openxcode = get_theme_mod('openx-code');
-		$sww_uri   = strtolower($_SERVER["REQUEST_URI"]);
-
-		if ( strpos( $sww_uri,'/top-100/' ) !== false )  {
-			$searchstr  = "div-gpt-ad-1398366141837";
-			$replacestr = "div-gpt-ad-1398366494877";
-			$openxcode  = str_ireplace($searchstr, $replacestr, $openxcode);
-
-			$searchstr  = "BIM_ROS";
-			$replacestr = "BIM_T100";
-			$openxcode  = str_ireplace($searchstr, $replacestr, $openxcode);
-		}
-
-		if (
-			strpos( $sww_uri,'/mdce/' ) !== false ||
-			strpos( $sww_uri,'/category/marine-dealer-conference/' ) !== false
-		) {
-			$searchstr  = "div-gpt-ad-1398366141837";
-			$replacestr = "div-gpt-ad-1398366605405";
-			$openxcode  = str_ireplace($searchstr, $replacestr, $openxcode);
-
-			$searchstr  = "BIM_ROS";
-			$replacestr = "BIM_MDCE";
-			$openxcode  = str_ireplace($searchstr, $replacestr, $openxcode);
-		}
-		?>
-
-		<div class="leaderboardAd">
-			<?php echo $openxcode; ?>
-		</div>
-
-	<?php } ?>
+			echo $openxcode;
+		} ?>
 
 	</div>
 
-<?php } ?>
+<?php }
